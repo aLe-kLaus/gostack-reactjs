e8b641aeb58b7bfac50dc9736373c4f62e0e6254
Alessom Klaus <alessomklaus13@gmail.com>
2020-12-24 09:55:05 -0300

    Listing api values using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header'
 import './App.css';
-import backgroundImage from './assets/fuscao.jpg';
+import api from './services/api';
 
 function App() {
-const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  });
 
   function userAddProject() {
     setProjects([...projects, `New Project ${Date.now()}`]);
@@ -14,9 +20,8 @@ const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end w
     <>
       <Header title="Projects" />
 
-      <img src={backgroundImage}/>
         <ul>
-          {projects.map(project => <li key='project'>{project}</li>)}
+          {projects.map(project => <li key={project.id}>{project.title}</li>)}
         </ul>
 
         <button type="button" onClick={userAddProject}>Add NewProject</button>
